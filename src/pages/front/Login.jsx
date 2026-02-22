4548acb73d9a69ba361569aa58475fa2de9f9615
srlun <qwe30108@gmail.com>
2026-02-22 15:16:10 +0800

    login新增

---
+++ src/pages/front/Login.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+function Login() {
+  return (
+    <div className=" bg-primary-50">
+      <div className="container mx-auto py-5">
+        <div className="row py-120">
+          <form className="login-form mx-auto rounded-5 bg-white col-10 col-lg-4 p-6">
+            {/* login title  */}
+            <div className="mb-5">
+              <h1 className="text-center fs-3 text-primary-950">登入帳號</h1>
+            </div>
+
+            {/* 帳密表單  */}
+            <div className="mb-3">
+              <label
+                htmlFor="username"
+                className="form-label text-center fs-4 text-primary-950"
+              >
+                帳號
+              </label>
+              <input
+                type="text"
+                className="form-control  fs-5 text-gray-700"
+                id="username"
+                placeholder="請輸入帳號"
+              />
+            </div>
+            <div className="mb-3">
+              <label
+                htmlFor="userpassword"
+                className="form-label text-center fs-4 text-primary-950"
+              >
+                密碼
+              </label>
+              <input
+                type="password"
+                className="form-control  fs-5 text-gray-700"
+                id="userpassword"
+                placeholder="請輸入密碼"
+              />
+            </div>
+
+            {/* 記住我  */}
+            <div className="form-check d-flex align-items-center gap-2 mb-3">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value=""
+                id="remember-checkbox"
+              />
+              <label
+                className="form-check-label  fs-5 text-gray-700"
+                htmlFor="remember-checkbox"
+              >
+                記住我的資料
+              </label>
+            </div>
+
+            {/* login button  */}
+            <div className="text-center">
+              <button
+                type="submit"
+                className="btn rounded-pill px-5 px-9 fs-5 bg-secondary-700 text-white"
+              >
+                登入
+              </button>
+            </div>
+
+            {/* forget password  */}
+            <div className="text-center mt-3">
+              <a href="#" className="fs-7 text-primary-900">
+                忘記密碼
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
